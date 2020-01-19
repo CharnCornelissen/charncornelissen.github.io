5444041ffeb3b1cc7f0eee5973e0a6abbc57d8f3
Charn Cornelissen <charn.corn@gmail.com>
2020-01-19 21:34:50 +0200

    Updates

---
+++ precache-manifest.9fc8571ac2a2b19e7568656c5f2b4c82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ec71b30a7bdd4981093cfd7815b9c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df01e553cc8a3564164b",
+    "url": "/static/css/main.88251843.chunk.css"
+  },
+  {
+    "revision": "44d91217a2106cc453b8",
+    "url": "/static/js/2.6fc8ad99.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6fc8ad99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df01e553cc8a3564164b",
+    "url": "/static/js/main.df0caef7.chunk.js"
+  },
+  {
+    "revision": "7dfb2d13f2b813ad7998",
+    "url": "/static/js/runtime-main.580e81a6.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
